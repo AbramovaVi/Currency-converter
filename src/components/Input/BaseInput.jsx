6555821ed8b2b80f2cc1e4af37825846cd6c644a
AbramovaVi <iri559159192005@icloud.com>
2022-02-20 23:29:47 +0300

    Minor fixes
    - add inputs validation because in some browsers type 'number' doesn't work correctly

--- src/components/Input/BaseInput.jsx
+++ src/components/Input/BaseInput.jsx
@@ -1,18 +1,26 @@
 import { connect } from "react-redux";
 import { setUserInput, setConvertInput } from '../../actions';
 import { TextField } from "@mui/material";
+import { useState } from "react";
 
 const BaseInput = ({ userInput, setUserInput }) => {
+    const [error, setError] = useState(false)
 
     const changeHandler = e => {
         setUserInput(e.target.value);
+        (isNaN(e.target.value)) ?  setError(true) : setError(false);
     }
     return (
-        <TextField id="standard-basic" variant="standard" type="number"
-                   placeholder="0"
-                   value={userInput}
-                   onChange={changeHandler}
-                   sx = {{ width: '95%', margin: 'auto', fontSize: '36px'}}
+        <TextField
+            id="standard-basic"
+            error = {error}
+            variant="standard"
+            type="number"
+            placeholder="0"
+            helperText={ error ? 'Введите число' : ' '}
+            value={userInput}
+            onChange={changeHandler}
+            sx = {{ width: '95%', margin: 'auto', fontSize: '36px'}}
         />
     )
 }
--- src/components/Input/ConvertInput.jsx
+++ src/components/Input/ConvertInput.jsx
@@ -1,19 +1,27 @@
 import { connect } from "react-redux";
 import { setConvertInput } from "../../actions";
 import { TextField } from "@mui/material";
+import {useState} from "react";
 
 const ConvertInput = ({userInputConvert, setConvertInput}) => {
+    const [error, setError] = useState(false)
 
     const changeHandler = e => {
         setConvertInput(e.target.value);
+        (isNaN(e.target.value)) ?  setError(true) : setError(false);
     }
 
     return (
-        <TextField id="standard-basic" variant="standard" type="number"
-                   value={userInputConvert || ''}
-                   placeholder="0"
-                   onChange={changeHandler}
-                   sx = {{ width: '95%', margin: 'auto', fontSize: '36px'}}
+        <TextField
+            id="standard-basic"
+            variant="standard"
+            error = {error}
+            helperText={ error ? 'Введите число' : ' '}
+            type="number"
+            value={ userInputConvert || '' }
+            placeholder="0"
+            onChange={changeHandler}
+            sx = {{ width: '95%', margin: 'auto', fontSize: '36px'}}
         />
     )
 }
